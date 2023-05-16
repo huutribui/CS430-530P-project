425d2e7dd852bea4f168f8bedb46ccbdfc8f38c0
huutribui <huu-tri.bui@intel.com>
2023-05-16 16:56:10 -0700

    Initialized pages folder with simple files

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutUs = () => {
+    return (
+        <div>
+            This is the About Us page
+        </div>
+    );
+};
+
+export default AboutUs;
---
+++ src/pages/ForecastPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ForecastPage = () => {
+    return (
+        <div>
+            This is the forcast page
+        </div>
+    );
+};
+
+export default ForecastPage;
