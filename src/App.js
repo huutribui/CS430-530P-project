911218007460499dc77cd85dfdbb70106fe95062
huutribui <huu-tri.bui@intel.com>
2023-05-16 17:07:56 -0700

    Initialized router and links for pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Home from './pages/Home';
+import ForecastPage from './pages/ForecastPage';
+import AboutUs from './pages/AboutUs';
 function App() {
   return (
     <div className="App">
-      React app
+      <Router>
+          <Switch>
+            <Route exact path="/" element={<Home/>} />
+            <Route exact path="/forecast" element={<ForecastPage/>} />
+            <Route exact path="/aboutUs" element={<AboutUs/>} />
+          </Switch>
+      </Router>
     </div>
   );
 }
