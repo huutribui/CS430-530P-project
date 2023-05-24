3570f26bde4d23c68033e77b7944ee03527b2188
huutribui <huu-tri.bui@intel.com>
2023-05-23 18:18:16 -0700

    Pushing previous changes to main

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+import {
+	BrowserRouter as Router,
+	Routes as Switch,
+	Route,
+	Redirect,
+} from 'react-router-dom';
+import Home from './pages/Home';
+import ForecastPage from './pages/ForecastPage';
+import AboutUs from './pages/AboutUs';
+import NavBar from './components/NavBar';
+function App() {
+	return (
+		<div className="App">
+			<Router>
+				<NavBar />
+				<div className='App-container'>
+					<Switch>
+						<Route exact path="/" element={<Home />} />
+						<Route exact path="/forecast" element={<ForecastPage />} />
+						<Route exact path="/aboutUs" element={<AboutUs />} />
+					</Switch>
+				</div>
+			</Router>
+		</div>
+	);
+}
+
+export default App;
