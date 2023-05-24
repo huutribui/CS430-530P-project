d888bef6f1b70da55f44fedf02f02c74ca52cac6
KSKRLIFE <konduru@pdx.edu>
2023-05-23 18:07:56 -0700

    Aboutus-pagelayout
    Updated the Aboutus page with basic stricture and background

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-
+import './AboutUs.css';
+import './Aboutusbackgr.png';
 const AboutUs = () => {
     return (
-        <div>
-            This is the About Us page and 
+        <div class="image">
+        <h1>About Us</h1>
+        <p>Welcome to our website!</p>
+        <div class="container">
+         This is Kishore.
+        </div>
+        <div class="container">
+         This is Nathan.
+        </div>
+        <p>We are a team of passionate web developers.</p>
         </div>
     );
 };
 
 export default AboutUs;
+
+
+
