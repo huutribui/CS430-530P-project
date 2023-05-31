d1dc9fe094e743d403072b3d71a2f4f657ca1b0d
huutribui <huu-tri.bui@intel.com>
2023-05-31 00:07:30 -0700

    Implemented max min graph

--- src/pages/ForecastPage.jsx
+++ src/pages/ForecastPage.jsx
@@ -13,6 +13,7 @@ import {
 	Title,
 	Tooltip,
 	Legend,
+	Colors,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 import './ForecastPage.css';
@@ -28,15 +29,19 @@ ChartJS.register(
 	LineElement,
 	Title,
 	Tooltip,
-	Legend
+	Legend,
+	Colors
 );
 
+ChartJS.defaults.color = 'snow';
+ChartJS.defaults.font.size = 16;
+ChartJS.defaults.font.weight = 700;
 const ForecastPage = () => {
 	const context = useContext(InfoContext);
 	const [city, setCity] = useState(context.city);
 	const [tempUnit, setTempUnit] = useState(context.tempUnit);
-	const [data24Hours, setData24Hours] = useState({});
-	const [currentData, setCurrentData] = useState({});
+	const [data24Hours, setData24Hours] = useState(null);
+	const [currentData, setCurrentData] = useState(null);
 
 	useEffect(() => {
 		getCurrentCityData(city);
@@ -79,6 +84,93 @@ const ForecastPage = () => {
 		setData24Hours(response.data);
 	};
 
+    const getHoursLabel = (list) => {
+        let label = [];
+        list.forEach(data => {
+            let tempHours = new Date((data.dt * 1000)).toLocaleTimeString([], {
+				hour: '2-digit'
+			});
+            label.push(tempHours);
+        })
+        label.unshift("Now");
+        return label;
+    }
+
+    const getMaxTemp24Hours = (list, currentData) => {
+        let maxtempList = [];
+        list.forEach(data => {
+            maxtempList.push(data.main.temp_max);
+        })
+        maxtempList.unshift(currentData?.main?.temp_max);
+        return maxtempList;
+    }
+
+    const getMinTemp24Hours = (list, currentData) => {
+        let mintempList = [];
+        list.forEach(data => {
+            mintempList.push(data.main.temp_min);
+        })
+        mintempList.unshift(currentData?.main?.temp_min);
+        return mintempList;
+    }
+
+	const renderLineChart = () => {
+		const options = {
+			plugins: {
+				legend: {
+					position: 'top',
+				},
+				colors: {
+					enabled: true,
+				},
+			},
+			scales: {
+				x: {
+					color: 'snow', // Set the color for the x-axis labels,
+					grid: {
+						color: 'rgba(255, 250, 250, 0.2)', // Set the color for vertical grid lines
+					},
+					border: {
+						display: true,
+					},
+				},
+				y: {
+					color: 'snow', // Set the color for the y-axis labels, // Set the color for the x-axis labels,
+					grid: {
+						color: 'rgba(255, 250, 250, 0.2)', // Set the color for vertical grid lines
+					},
+                    beginAtZero: true // Begin the y-axis at zero
+				},
+			},
+			layout: {
+				padding: 0,
+			},
+			responsive: true,
+		};
+
+		const labels = getHoursLabel(data24Hours.list);
+		const data = {
+			labels,
+			datasets: [
+				{
+					label: 'max temperature',
+					borderWidth: 2, // Set the line width
+					data: getMaxTemp24Hours(data24Hours.list, currentData),
+					borderColor: 'rgb(255, 99, 132)',
+					backgroundColor: 'rgba(255, 99, 132, 0.5)',
+				},
+				{
+					label: 'min temperature',
+					borderWidth: 2, // Set the line width
+					data: getMinTemp24Hours(data24Hours.list, currentData),
+					borderColor: 'rgb(53, 162, 235)',
+					backgroundColor: 'rgba(53, 162, 235, 0.5)',
+				},
+			],
+		};
+		return <Line data={data} options={options} />;
+	};
+
 	return (
 		<div className="forecastPage">
 			<div>
@@ -95,9 +187,11 @@ const ForecastPage = () => {
 				</p>
 			</div>
 
-			<div className="forecast-container"></div>
-			<div className="forecast-container"></div>
-			<div className="forecast-container"></div>
+			<div className="minmaxTempForecast">
+                <div className="graphLabel">Min Max Temperature Graph for the next 24 Hours</div>
+                {data24Hours && renderLineChart()}
+            </div>
+            
 		</div>
 	);
 };
