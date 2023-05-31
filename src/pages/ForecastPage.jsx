38079288b88892d73de5fd73337aaffb558cf476
huutribui <huu-tri.bui@intel.com>
2023-05-30 21:22:09 -0700

    Extracted 24hs and current data for forecast graphs

--- src/pages/ForecastPage.jsx
+++ src/pages/ForecastPage.jsx
@@ -18,6 +18,9 @@ import { Line } from 'react-chartjs-2';
 import './ForecastPage.css';
 import { API_KEY } from '../config';
 
+const FAHRENHEIT_UNIT = 'imperial';
+const CELSIUS_UNIT = 'metric';
+
 ChartJS.register(
     CategoryScale,
     LinearScale,
@@ -33,26 +36,65 @@ const ForecastPage = () => {
     const [city, setCity] = useState(context.city);
     const [tempUnit, setTempUnit] = useState(context.tempUnit);
     const [data24Hours, setData24Hours] = useState({});
+    const [currentData, setCurrentData] = useState({});
 
-    const extractHourData = (dates) => {
+    useEffect(() => {
+		getCurrentCityData(city);
+        getDatafor24Hours();
+	}, [tempUnit]);
 
-    }
+    const changeToCelciusUnit = (event) => {
+		event.preventDefault();
+		event.stopPropagation();
+		setTempUnit(CELSIUS_UNIT);
+		context.changeTempUnit(CELSIUS_UNIT);
+	};
+
+	const changeToFahrenheitUnit = (event) => {
+		event.preventDefault();
+		event.stopPropagation();
+		setTempUnit(FAHRENHEIT_UNIT);
+		context.changeTempUnit(FAHRENHEIT_UNIT);
+	};
+
+    const getCurrentCityData = (city) => {
+		const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${tempUnit}`;
+		axios
+			.get(url)
+			.then((response) => {
+				console.log('getCurrentCityData response: ', response.data);
+				setCurrentData(response.data);
+			})
+			.catch((err) => {
+				console.log('ERROR getCityData: ', err);
+				alert("Error message getting city's data");
+			});
+		console.log('getCityData', city);
+	};
 
     const getDatafor24Hours = async () => {
         const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=${tempUnit}&cnt=8`;
         let response = await axios.get(url);
-        console.log("response: ", response.data);
+        console.log("getDatafor24Hours response: ", response.data);
+        setData24Hours(response.data);
     }
 
-    useEffect(  () => {
-        getDatafor24Hours();
-    })
-
     return (
-        <div>
+        <div className="forecastPage">
         <div>
         <input type="search" placeholder="Search" className='search'/>
         </div>
+
+        <div className="degreeUnits">
+				<p className="degreeUnit" onClick={changeToCelciusUnit}>
+					&deg;C
+				</p>
+
+				<p className="degreeUnit" onClick={changeToFahrenheitUnit}>
+					&deg;F
+				</p>
+        </div>
+
         <div className="forecast-container">
          
         </div>
