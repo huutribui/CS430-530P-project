c8eb9e96cd91a679d21a783294ba88614015b5a0
huutribui <huu-tri.bui@intel.com>
2023-05-30 21:34:19 -0700

    formatted with prettier

--- src/pages/ForecastPage.jsx
+++ src/pages/ForecastPage.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import {useState} from 'react';
-import {useEffect} from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { useContext } from 'react';
 import { InfoContext } from '../components/ContextProvider.js';
 import axios from 'axios';
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
+	Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 import './ForecastPage.css';
@@ -22,28 +22,28 @@ const FAHRENHEIT_UNIT = 'imperial';
 const CELSIUS_UNIT = 'metric';
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
+	Legend
 );
 
 const ForecastPage = () => {
-    const context = useContext(InfoContext);
-    const [city, setCity] = useState(context.city);
-    const [tempUnit, setTempUnit] = useState(context.tempUnit);
-    const [data24Hours, setData24Hours] = useState({});
-    const [currentData, setCurrentData] = useState({});
+	const context = useContext(InfoContext);
+	const [city, setCity] = useState(context.city);
+	const [tempUnit, setTempUnit] = useState(context.tempUnit);
+	const [data24Hours, setData24Hours] = useState({});
+	const [currentData, setCurrentData] = useState({});
 
-    useEffect(() => {
+	useEffect(() => {
 		getCurrentCityData(city);
-        getDatafor24Hours();
+		getDatafor24Hours();
 	}, [tempUnit]);
 
-    const changeToCelciusUnit = (event) => {
+	const changeToCelciusUnit = (event) => {
 		event.preventDefault();
 		event.stopPropagation();
 		setTempUnit(CELSIUS_UNIT);
@@ -57,7 +57,7 @@ const ForecastPage = () => {
 		context.changeTempUnit(FAHRENHEIT_UNIT);
 	};
 
-    const getCurrentCityData = (city) => {
+	const getCurrentCityData = (city) => {
 		const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${tempUnit}`;
 		axios
 			.get(url)
@@ -72,20 +72,20 @@ const ForecastPage = () => {
 		console.log('getCityData', city);
 	};
 
-    const getDatafor24Hours = async () => {
-        const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=${tempUnit}&cnt=8`;
-        let response = await axios.get(url);
-        console.log("getDatafor24Hours response: ", response.data);
-        setData24Hours(response.data);
-    }
+	const getDatafor24Hours = async () => {
+		const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=${tempUnit}&cnt=8`;
+		let response = await axios.get(url);
+		console.log('getDatafor24Hours response: ', response.data);
+		setData24Hours(response.data);
+	};
 
-    return (
-        <div className="forecastPage">
-        <div>
-        <input type="search" placeholder="Search" className='search'/>
-        </div>
+	return (
+		<div className="forecastPage">
+			<div>
+				<input type="search" placeholder="Search" className="search" />
+			</div>
 
-        <div className="degreeUnits">
+			<div className="degreeUnits">
 				<p className="degreeUnit" onClick={changeToCelciusUnit}>
 					&deg;C
 				</p>
@@ -93,19 +93,13 @@ const ForecastPage = () => {
 				<p className="degreeUnit" onClick={changeToFahrenheitUnit}>
 					&deg;F
 				</p>
-        </div>
+			</div>
 
-        <div className="forecast-container">
-         
-        </div>
-        <div className="forecast-container">
-         
-        </div>
-        <div className="forecast-container">
-         
-        </div>
-        </div>
-    );
+			<div className="forecast-container"></div>
+			<div className="forecast-container"></div>
+			<div className="forecast-container"></div>
+		</div>
+	);
 };
 
 export default ForecastPage;
